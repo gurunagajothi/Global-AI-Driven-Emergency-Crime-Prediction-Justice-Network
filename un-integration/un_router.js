7920571a48370f17a3f2b967777cd17e17766965
Guru Nagajothi S <gurunagajothi2007@gmail.com>
2026-01-11 18:14:42 +0530

    Delete un-integration directory

--- un-integration/un_router.js
+++
@@ -1,9 +0,0 @@
-const protocols = require("./protocols.json");
-
-function routeToUN(type) {
-  const protocol = protocols[type];
-  console.log("🇺🇳 UN PROTOCOL ACTIVATED");
-  console.log(protocol);
-}
-
-module.exports = routeToUN;
