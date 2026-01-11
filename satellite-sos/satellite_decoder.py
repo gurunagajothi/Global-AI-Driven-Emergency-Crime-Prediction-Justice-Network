f93c6da04a43f78fc5f39d77130340858a711831
Guru Nagajothi S <gurunagajothi2007@gmail.com>
2026-01-11 18:13:54 +0530

    Delete satellite-sos directory

--- satellite-sos/satellite_decoder.py
+++
@@ -1,5 +0,0 @@
-def decode(message):
-    parts = message.split("|")
-    return {"type": parts[0], "lat": parts[1], "lon": parts[2]}
-
-print(decode("SOS|12.97|77.59"))
--- satellite-sos/satellite_encoder.py
+++
@@ -1,4 +0,0 @@
-def encode_sos(lat, lon):
-    return f"SOS|{lat}|{lon}"
-
-print(encode_sos(12.9716, 77.5946))
